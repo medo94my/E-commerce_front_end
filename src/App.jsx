ea3d7cba9c387713de397ddd47534f74c3ff2821
medmedo94my <medoroyalrma@gmail.com>
2022-04-18 23:02:44 +0800

    backend first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+
+function App() {
+  return (
+    <Cart/>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import { ShoppingCartOutlined } from '@mui/icons-material';
+import {mobile} from "../responsive"
+const Container=styled.div`
+height:60px;
+width:100%;
+${mobile({height:"50px"})}
+
+`
+const Wrapper= styled.div`
+padding:10px 20px;
+display :flex;
+justify-content:space-between;
+align-items: center;
+${mobile({padding:"10px 0px"})}
+`
+const Left=styled.div`
+flex:1;
+display:flex;
+align-items:center;
+`
+const Language=styled.span`
+font-size:14px;
+cursor:pointer;
+${mobile({display:"none"})}
+`
+const SearchContainer=styled.div`
+border:0.5px solid gray;
+display:flex;
+align-items: center;
+margin-left:25px;
+`
+const Input=styled.input`
+border:none;
+${mobile({width:"50px"})}
+`
+const Center=styled.div`
+flex:1;
+text-align:center;
+`
+const Logo=styled.h1`
+font-weight:bold;
+${mobile({fontSize:"24px"})}
+`
+const Right=styled.div`
+flex:1;
+display:flex;
+align-items:center;
+justify-content:flex-end;
+${mobile({justifyContent:"center",flex:2})}
+`
+
+const MenuItem=styled.div`
+font-size:14px;
+cursor:pointer;
+margin-left:25px;
+${mobile({fontSize:"12px",marginLeft:"10px"})}
+`
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                    <Input placeholder='Sreach'/>
+                    <SearchIcon style={{color:'gray',fontSize:16}}/>
+                </SearchContainer>
+            </Left>
+            <Center><Logo>HishaM.</Logo></Center>
+            <Right>
+                <MenuItem>Register</MenuItem>
+                <MenuItem>LOGIN</MenuItem>
+                <MenuItem>
+                
+                <Badge badgeContent={4} color="primary">
+      <ShoppingCartOutlined color="action" />
+    </Badge>
+                </MenuItem>
+    </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
